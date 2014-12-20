f6c6cb49b9f508d75262d099e81bd4c71a9db46f
fanqie93 <1339070342@qq.com>
2014-12-20 12:17:16 +0800

    1.64_fan
    首页-导航-banner

--- Public/Home/js/banner.js
+++ Public/Home/js/banner.js
@@ -6,20 +6,23 @@ $(function(){
 			var myFn = function()
 			{
     			num++;
-				if(num > $('ol li').length - 1){ num = 0;}
+				if(num > $('.banner ol li').length -1){  num = 0;}
 				$('.banner ol li').eq(num).addClass('current').siblings().removeClass('current');
-				$('.banner ul li').eq(num).fadeIn().siblings().hide();
+				$('.banner ul li').eq(num).stop().fadeIn().siblings().fadeOut();
     		}
-			timer =setInterval(myFn,2000);
+			timer =setInterval(myFn,2500);
+			
+			$('.banner').mouseover(function(e) {
+                clearInterval(timer);
+				timer =null;
+            }).mouseout(function(e) {
+				clearInterval(timer);
+                timer = setInterval(myFn,2500);
+            });
+			
     		$('.banner ol li').mouseover(function(e) {
-                $('.banner ul li').eq($(this).index()).fadeIn().siblings().hide();
+                $('.banner ul li').eq($(this).index()).stop().fadeIn().siblings().fadeOut();
 				$(this).addClass('current').siblings().removeClass('current');
 				num = $(this).index();
             });
-			$('.banner').mouseover(function(e) {
-                clearInterval(timer);
-            }).mouseout(function(e) {
-				clearInterval(timer);
-                timer = setInterval(myFn,2000);
-            });;
     	})
