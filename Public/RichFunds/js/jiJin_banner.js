a61b9edd21e60b4885b0db024a39c8ff302d1975
fanqie93 <1339070342@qq.com>
2014-12-20 16:03:22 +0800

    1.70.1_f
    基金

--- Public/RichFunds/js/jiJin_banner.js
+++ Public/RichFunds/js/jiJin_banner.js
@@ -7,11 +7,11 @@ $(function(){
     			num++;
 				if(num > $('ol li').length - 1){ num = 0;}
 				$('.banner ol li').eq(num).addClass('current').siblings().removeClass('current');
-				$('.banner .ban li').eq(num).fadeIn().siblings().hide();
+				$('.banner .ban li').eq(num).stop().fadeIn().siblings().fadeOut();
     		}
 			timer =setInterval(myFn,2500);
     		$('.banner ol li').mouseover(function(e) {
-                $('.banner .ban li').eq($(this).index()).fadeIn().siblings().hide();
+                $('.banner .ban li').eq($(this).index()).stop().fadeIn().siblings().fadeOut();
 				$(this).addClass('current').siblings().removeClass('current');
 				num = $(this).index();
             });
