5d96ca2873724d43a5abdae69ba109076745d083
fanqie93 <1339070342@qq.com>
2014-12-30 16:38:09 +0800

    2.01_f
    jijinxiangqing.js

--- Public/RichFunds/js/jijinxiangqing.js
+++ Public/RichFunds/js/jijinxiangqing.js
@@ -2,11 +2,15 @@
 $(function(){
 	 //alert($(window).width());
 	$(document).scroll(function(){
+		//alert($(document).scrollTop());
+		//if($(document).scrollTop() > $('.bar_out').height()+$('.nav_out').height()+$('.jjxq_ban').height() && $(document).scrollTop() <$('.bar_out').height()+$('.nav_out').height()+$('.jjxq_ban').height()+$('.jjxq').height()-1000)
 		if($(document).scrollTop() > $('.bar_out').height()+$('.nav_out').height()+$('.jjxq_ban').height())
 		{
+			$('.jjxq .jjxq_r .jjxq_r_top03').css('display','none');
 			if($(window).width() >1600)
 			{
 				$('.jjxq .jjxq_r').css({'position':'fixed','top':'0','left':'59%'});
+				
 			}
 			else
 			{
@@ -15,6 +19,7 @@ $(function(){
 		}
 		else
 		{
+			$('.jjxq .jjxq_r .jjxq_r_top03').css('display','block');
 			$('.jjxq .jjxq_r').css({'position':'static'});
 		}
 	});	
