a0c25412f2bf605cf9be20e27d26825459e033bd
huizheshijie <380003832@qq.com>
2015-01-09 17:48:29 +0800

    2.171_h
    后台代码填写

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -31,7 +31,8 @@ return array(
 
     /* 数据库配置 */
     'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => '59.188.8.43', // 服务器地址
+    // 'DB_HOST'   => '59.188.8.43', // 服务器地址
+    'DB_HOST'   => '127.0.0.1', // 服务器地址
     'DB_NAME'   => 'gica', // 数据库名
     'DB_USER'   => 'root', // 用户名
     'DB_PWD'    => 'root',  // 密码
