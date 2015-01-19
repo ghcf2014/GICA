91d91ac4e3d4fad9ded69980734b0a2090737fe2
colinphp <1165407848@qq.com>
2015-01-19 17:04:49 +0800

    3.4_colin
    工合基金链接修改，首页链接修改

--- Application/Home/Controller/NewhelpController.class.php
+++ Application/Home/Controller/NewhelpController.class.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Home\Controller;
+
 use OT\DataDictionary;
 
 /**
@@ -8,102 +9,87 @@ use OT\DataDictionary;
  * 主要获取首页聚合数据
  */
 class NewhelpController extends HomeController {
-
-	//系统首页
-    public function index(){
-
-        $this->display();
-    }
-    public function borrowhelp(){
-
-        $this->display();
-    }
-        public function companyintr(){
-
-        $this->display();
-    }
-        public function compare_tools(){
-
-        $this->display();
-    }
-        public function compare_tools_swf(){
-
-        $this->display();
-    }
-    
-        public function cpi_tools(){
-
-        $this->display();
-    }
-        public function cusservice(){
-
-        $this->display();
-    }
-        public function forumanswer(){
-
-        $this->display();
-    }
-        public function forumquestion(){
-
-        $this->display();
-    }
-        public function forumwaitquestion(){
-
-        $this->display();
-    }
-        public function income_tools(){
-
-        $this->display();
-    }
-        public function legalpolicy(){
-
-        $this->display();
-    }
-        public function linkship(){
-
-        $this->display();
-    }
-        public function myforumquestion(){
-
-        $this->display();
-    }
-        public function newguide(){
-
-        $this->display();
-    }
-        public function question(){
-
-        $this->display();
-    }
-        public function safeguard(){
-
-        $this->display();
-    }
-        public function share(){
-
-        $this->display();
-    }
-        public function social_tool(){
-
-        $this->display();
-    }
-        public function social_tools(){
-
-        $this->display();
-    }
-        public function tenthousand(){
-
-        $this->display();
-    }
-        public function terracetheory(){
-
-        $this->display();
-    }
-
-    /**
-     * 关于我们
-     */
-    public function aboutus(){
-    	$this->display();
-    }
+	
+	// 系统首页
+	public function index() {
+		$this->display ();
+	}
+	public function borrowhelp() {
+		$this->display ();
+	}
+	public function companyintr() {
+		$this->display ();
+	}
+	public function compare_tools() {
+		$this->display ();
+	}
+	public function compare_tools_swf() {
+		$this->display ();
+	}
+	public function cpi_tools() {
+		$this->display ();
+	}
+	public function cusservice() {
+		$this->display ();
+	}
+	public function forumanswer() {
+		$this->display ();
+	}
+	public function forumquestion() {
+		$this->display ();
+	}
+	public function forumwaitquestion() {
+		$this->display ();
+	}
+	public function income_tools() {
+		$this->display ();
+	}
+	public function legalpolicy() {
+		$this->display ();
+	}
+	public function linkship() {
+		$this->display ();
+	}
+	public function myforumquestion() {
+		$this->display ();
+	}
+	public function newguide() {
+		$this->display ();
+	}
+	public function question() {
+		$this->display ();
+	}
+	public function safeguard() {
+		$this->display ();
+	}
+	public function share() {
+		$this->display ();
+	}
+	public function social_tool() {
+		$this->display ();
+	}
+	public function social_tools() {
+		$this->display ();
+	}
+	public function tenthousand() {
+		$this->display ();
+	}
+	public function terracetheory() {
+		$this->display ();
+	}
+	
+	/**
+	 * 关于我们
+	 */
+	public function aboutus() {
+		$this->display ();
+	}
+	// 网站服务协议
+	public function fuwutiaokuan() {
+		$this->display ();
+	}
+	// 网站使用条款
+	public function shiyongtiaokuan() {
+		$this->display ();
+	}
 }
