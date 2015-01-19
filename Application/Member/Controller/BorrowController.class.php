ffbdb791d1c0efe24b4a478ebf4388edd18ea689
colinphp <1165407848@qq.com>
2015-01-19 21:50:12 +0800

    3.513_colin
    借款管理（包括全部借款，招标中的借款，等）

--- Application/Member/Controller/BorrowController.class.php
+++ Application/Member/Controller/BorrowController.class.php
@@ -18,11 +18,64 @@ class BorrowController extends MemberController {
 	 * @author liuy
 	 *         2015-1-15我的借款，包括成功的借款，未成功的借款，回收的借款。
 	 */
-	public function borrowmanager() {
+	public function myborrowmanager() {
 		$uid = is_login (); // 获取当前用户UID
 		$borrow_info = M ( 'z_borrow_info' );
 		$condition ['borrow_uid'] = $uid;
 		$borrow_info = $borrow_info->where ( $condition )->select ();
+		$this->assign ( 'list', $borrow_info );
+		$this->display ();
+	}
+	
+	/**
+	 *
+	 * @author liuy
+	 *         2015-1-19审核中的借款(包括初审中的借款以及复审中的借款)
+	 */
+	public function checkingborrow() {
+		$uid = is_login (); // 获取当前用户UID
+		$borrow_info = M ( 'z_borrow_info' );
+		$sql = "select * from gica_z_borrow_info where borrow_uid=" . $uid . " and borrow_status=0 or borrow_status=4";
+		$borrow_info = $borrow_info->query ( $sql );
+		$this->assign ( 'list', $borrow_info );
+		$this->display ();
+	}
+	
+	/**
+	 *
+	 * @author liuy
+	 *         2015-1-19招标中的借款
+	 */
+	public function issueborrow() {
+		$uid = is_login (); // 获取当前用户UID
+		$borrow_info = M ( 'z_borrow_info' );
+		$borrow_info = $borrow_info->where ( 'borrow_uid=' . $uid . " and borrow_status=2" )->select ();
+		$this->assign ( 'list', $borrow_info );
+		$this->display ();
+	}
+	
+	/**
+	 *
+	 * @author liuy
+	 *         2015-1-15借款管理，成功的借款数据查询页
+	 */
+	public function borrowmanager() {
+		$uid = is_login (); // 获取当前用户UID
+		$borrow_info = M ( 'z_borrow_info' );
+		$borrow_info = $borrow_info->where ( "borrow_uid=" . $uid . " and borrow_status=7" )->select ();
+		$this->assign ( 'list', $borrow_info );
+		$this->display ();
+	}
+	
+	/**
+	 *
+	 * @author liuy
+	 *         2015-1-15借款管理，还款中的借款
+	 */
+	public function repaymentborrow() {
+		$uid = is_login (); // 获取当前用户UID
+		$borrow_info = M ( 'z_borrow_info' );
+		$borrow_info = $borrow_info->where ( "borrow_uid=" . $uid . " and borrow_status=6" )->select ();
 		$this->assign ( 'list', $borrow_info );
 		$this->display ();
 	}
