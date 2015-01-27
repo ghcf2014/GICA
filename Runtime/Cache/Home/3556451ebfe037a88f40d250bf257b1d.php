42138e35eb0befc70878433d6f137631b7cc3fef
colinphp <1165407848@qq.com>
2015-01-27 19:59:38 +0800

    4.02_colin
    个人中心左边栏链接修改

---
+++ Runtime/Cache/Home/3556451ebfe037a88f40d250bf257b1d.php
@@ -0,0 +1,59 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>工合基金  网站服务协议</title>
+		<link rel="stylesheet" type="text/css" href="/Public/Home/css/style.css"/>
+	</head>
+	<body>
+		<div class="fwxy">
+			<h2>工合财富网站服务协议</h2>
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;尊敬的用户，欢迎您注册成为本网站用户。在注册前请您仔细阅读如下服务协议：本服务协议双方为本网站与本网站用户，本服务协议具有合同效力。您确认本服务协议后，本服务协议即在您和本网站之间产生法律效力。请您务必在注册之前认真阅读全部服务协议内容，如有任何疑问，可向本网站咨询。</p>
+			
+			<h3>一、接受协议</h3>
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;您申请注册为工合财富用户，表明您已经充分阅读、理解并同意自己已经与工合财富订立本协议，且您将受本协议的条款和条件约束。工合财富有权决定修订本协议，如有任何修订，工合财富将其网站上刊载公告，经修订的协议一经在工合财富网站公布后，立即自动生效。您登录或继续使用工合财富服务将表示您接受经修订的协议。</p>
+			
+			<h3>二、用户资格</h3>
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;工合财富的用户应当为有完全民事行为能力的自然人、法人、其他组织。</p>
+			
+			<h3>三、注册义务</h3>
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;在工合财富网站注册为用户须同意：<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1. 提供真实、准确、完整的信息；<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2. 及时更新会员信息。倘若存在用户信息不真实、不准确、不完整的情形，或工合财富有合理理由怀疑用户信息不真实、不准确、不完整，工合财富有权暂停或终止用户资格。</p>
+			
+			<h3>四、会员注册名、密码和保密</h3>
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1、会员注册<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;（1）年满18周岁，具备完全民事行为能力的自然人均可在工合财富网站注册（同一证件仅限注册一个账户）。<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;（2）按照网站注册指引流程完成手机绑定、实名认证、银行卡绑定后即成为网站会员，会员分为普通会员和VIP会员。<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;（3）普通会员仅可享受网站基本功能，在缴纳VIP费后可升级成为VIP会员，VIP会员可以享受网站专属客服服务、风险准备金全额垫付、自动投标、天标投资、债权转让、等增值服务和权利。<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;在注册过程中，您将设置用户注册名和密码，并须对密码进行保密。您同意：<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1. 如发现任何人未经授权使用您的用户注册名或密码，您将立即通知工合财富；<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2. 确保您每次按照正确步骤退出登录。工合财富不能也不会对因您未能遵守本协议而发生的任何损失负责。</p>
+			
+			<h3>五、 用户信息规则</h3>
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;用户须保证在工合财富网站的用户信息及行为：<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1. 不存在欺诈情形；<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2. 不侵犯第三方合法权益；<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;3. 不违反法律规定；<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4. 不含有诽谤、恐吓、骚扰的内容；<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;5. 不含有淫秽、色情内容；<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;6. 不恶意攻击工合财富网站。</p>
+			
+			<h3>六、服务和费用</h3>
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;服务<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;工合财富为用户提供投融资服务，包括审核融资项目、信息发布、撮合交易、资讯服务等。<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;费用<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;（1）免费注册为工合财富的用户。<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;（2）工合财富为用户提供的投融资服务将按照工合财富网站收费规则收取费用（具体参见《工合财富资费标准》）。<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;其他<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;（1）用户自行承担使用工合财富所提供服务过程中的其他支出，包括网络使用费、通信费等。<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;（2）用户在工合财富网站的投资收益自行承担税费。<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;（3）用户在投标后不允许取消。若所投标的流标，则冻结的资金将自行解冻，用户不收取资金冻结期间的利息。</p>
+			
+			<h3>七、隐私</h3>
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;工合财富根据《隐私保护》保护用户信息。</p>
+			
+			<h3>八、信用评价系统</h3>
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;工合财富建立用户信用评价系统，并根据用户在工合财富信用评级对用户进行管理。</p>
+			
+			<h3>九、违约责任</h3>
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;任何一方违反本协议约定，造成对方损失的，则应承担损害赔偿责任。<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;若用户违反本协议的，工合财富有权采取如下措施：<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;（1）限制、删除信息发布；<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;（2）警告；<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;（3）暂停、终止用户资格等。</p>
+			
+			<h3>十、追偿</h3>
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;如因用户责任导致工合财富向第三方承担了赔偿责任，则工合财富有权向用户追偿。</p>
+			
+			<h3>十一、通知</h3>
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;工合财富通知采取如下一种或多种方式：网站公告、向用户注册电子邮箱发送邮件、向用户注册手机发送短信或电话通知。<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;工合财富向用户发出的通知，按照以下方式确定送达时间：<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;（1）网站公告：公告发布之日；<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;（2）电子邮箱：工合财富发出电子邮件当日；<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;（3）手机短信/电话：工合财富发出短信或致电当日。</p>
+			
+			<h3>十二、一般规定</h3>
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 本协议适用中华人民共和国法律。<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;工合财富与用户之间的争议，由工合财富住所地法院管辖。<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 本协议的规定是可分割的，如本协议任何规定被裁定为无效或不可执行，该规定可被删除而其余条款应予以执行。</p>
+			
+			<h3>十三、生效</h3>
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;本协议在您提交的注册为工合财富用户的申请获得工合财富审核通过时生效。</p>
+			
+			<p style="text-align: right;margin-top: 15px;">工合基金<br />2015.01.20</p>
+			
+		</div>
+
+	</body>
+</html>
