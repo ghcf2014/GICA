5e2ac79b59118681a5b860b3971e0d85f493bd02
huizheshijie <380003832@qq.com>
2015-04-23 09:42:19 +0800

    5.42_h
    回潮支付/充值

--- Application/Member/Controller/IndexController.class.php
+++ Application/Member/Controller/IndexController.class.php
@@ -33,6 +33,11 @@ class IndexController extends MemberController {
         $lists2    = D('z_borrow_investor')->field('sum(investor_capital)investor_capital')->where($lists2_uid)->order('investor_capital desc')->group('investor_uid')->select();
         $lists3_uid['borrow_uid'] =$uid;
         $lists3    = D('z_borrow_info')->field('sum(borrow_money)borrow_money')->where($lists3_uid)->group('borrow_uid')->select();
+
+        //会员充值金额查询
+        $mstatus = M('z_member_payonline');//用户验证状态
+        $condition2['uid'] =$uid;
+        $paymoney=$mstatus->field('sum(money)money')->where($condition2)->group('uid')->select();
         
 
         $m = M('ucenter_member');//用户头像
@@ -44,6 +49,7 @@ class IndexController extends MemberController {
         $this->assign('borrow_money', $lists3);
         $this->assign('list3', $m);
         $this->assign('mstatus', $member_status);
+        $this->assign('paymoney', $paymoney);
 
         $arr['id']=$uid;
         $leveldata=M('ucenter_member');
