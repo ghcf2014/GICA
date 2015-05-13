332b6536f9fe0cb23f4e369fff063fc9d96f9479
yushuangxiang <yu_shaungxiang@sina.com>
2015-05-13 17:24:54 +0800

    6.203_y
    弹出框登录，头像调整，找回交易密码

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -401,15 +401,9 @@ class UserController extends HomeController {
             if($res['status']){
 
             	//发送站内信
-<<<<<<< HEAD
-	            $action='您于'.date('Y-m-d H:i:s',time()).'修改了密码。';
-	            $opertype=5;//系统通知
-	            $result_ms=inner_msg($uid,$opertype,$action);  
-=======
 
                 // $action="修改了登陆密码";
                 // system_msg($action);
->>>>>>> origin/master
                 
                 $this->success('修改密码成功！');
             }else{
