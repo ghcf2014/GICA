a0f6cb184dcf970e898868a455b15563f01409b8
yushuangxiang <yu_shaungxiang@sina.com>
2015-05-22 15:59:49 +0800

    6.555_y
    字段更新

--- Application/User/Conf/config.php
+++ Application/User/Conf/config.php
@@ -8,7 +8,7 @@ define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', 'Y9k>up5P=Aa([EFr|DsO0cdSH3Jm]$W+Qxi.g)~Z'); //加密KEY
 // define('UC_DB_DSN', ''.C('DATABASE_TYPE').'://'.C('DATABASE_USER').':'.C('DATABASE_PWD').'@'.C('DATABASE_HOST').':'.C('DATABASE_PORT').'/'.C('DATABASE_NAME').''); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_DB_DSN', ''.C('DB_TYPE').'://'.C('DB_USER').':'.C('DB_PWD').'@'.C('DB_HOST').':'.C('DB_PORT').'/'.C('DB_NAME').''); // 数据库连接，使用Model方式调用API必须配置此项
-define('UC_DB_DSN', 'mysql://root:root@59.188.8.43:3306/gica'); // 数据库连接，使用Model方式调用API必须配置此项
-// define('UC_DB_DSN', 'mysql://root:root@127.0.0.1:3306/gica'); // 数据库连接，使用Model方式调用API必须配置此项
+// define('UC_DB_DSN', 'mysql://root:root@59.188.8.43:3306/gica'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysql://root:root@127.0.0.1:3306/gica'); // 数据库连接，使用Model方式调用API必须配置此项
 // define('UC_TABLE_PREFIX', 'gica_'); // 数据表前缀，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', C('DB_PREFIX')); // 数据表前缀，使用Model方式调用API必须配置此项
